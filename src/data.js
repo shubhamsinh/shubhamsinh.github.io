d4e3ab30ae649c5b1b9a03ebca5c91bf11c34c68
shubhamsinh <shubhamrahevar@gmail.com>
2022-07-05 13:46:02 +0530

    Fixed image bug

--- src/data.js
+++ src/data.js
@@ -21,22 +21,22 @@ export const about = {
 export const products = [
   {
     id: 1,
-    img: "https://drive.google.com/file/d/1eg5eoLhU3ghoMBfcjaY5z_h62l4PZ6Pb/view?usp=sharing",
+    img: "https://lh3.googleusercontent.com/A0soygHuipo2DVvik3yzDxtUTPY8WaoShw-eDknJXgXY12S_ZHtSb0ovE3DrZANI-1UWD8_eklzc8YZmLLAltSSqwHKpr9gFTBFuj0wv_rEAR6TOT-obE333Q5KYIPhWAP-Eq6UuKw=w2400",
     link: "https://github.com/shubhamsinh/laundryuncle",
   },
   {
     id: 2,
-    img: "https://drive.google.com/file/d/1YeXu6rQxwpq37VJd-yiuaFbBpWaJXZfR/view?usp=sharing",
+    img: "https://lh3.googleusercontent.com/3GaDbU_OEL22JSOhWVIB88YvTawS3_nmkOgOkpVbGrSGycwmMXd5V1lZHyjU_OvF1nwjj3e6zkhuFJ-l_XOEK7NApT4_jBRTFztsdQF0P8x7gypKocw3TcfwBy8Giixx1LtycqNSVQ=w2400",
     link: "https://github.com/shubhamsinh/urbangreens",
   },
   {
     id: 3,
-    img: "https://drive.google.com/file/d/1fcLITnoOvHXI6YGgkE8Jl4JYEdQDQdME/view?usp=sharing",
+    img: "https://lh3.googleusercontent.com/BCiO82s4Xh-NdxmZfn3QaBDWqzHmDM2f8FOyf4b4afQkAB3M2i4UeZpdujYF-cSVoggsFNgZ-4UQJyNRx1fpmdSSkw9r20Yo4WwcOfZUo4-u33T0JOikMtAF64CuH4C_IOjY0eoAnA=w2400",
     link: "https://github.com/shubhamsinh/urbangreens",
   },
   {
     id: 4,
-    img: "https://drive.google.com/file/d/1YfJKIj4FohQ0iyDOledVxcp8TzLAcJRZ/view?usp=sharing",
+    img: "https://lh3.googleusercontent.com/s1wDm8qvICuMNILqmDydoYpWXsPi9MTpzxRxDW1IHQSH_dJ4Nedp2cpBLyZPYhFQiMVjY7I-WoRrYx7XH-VasmPQ8yy7AMEXI_azcJFQrL4ZhLrtdigf8pxKCUKv4x2nysCd7KB-3g=w2400",
     link: "https://github.com/shubhamsinh/text_recognition",
   },
 ];
